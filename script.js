c4b113eef0c845c2d5b19d05939e805899d8bf11
Trey <trey.shearin3@gmail.com>
2024-02-04 15:59:25 -0500

    nexted query URL within the getWeatherAPI function, generated fetch function, and returned response as json

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var APIKey = "79c8885bec0fbdcd642a8ba60c566561";
 var city;
 
+function getWeatherAPI(){
+
 var queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+fetch(queryURL)
+.then(function (response){
+    return response.json();
+})
+.then(function (data){
+
+});
+}
